c0302b51b374af728847d097174650e9b7b3a828
Colby Fox <colbyfox@byu.edu>
2026-09-17 21:08:15 -0600

    implemented the PieceMoves in ChessPiece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,7 +53,20 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece piece = board.getPiece(myPosition);
+
+        if (piece == null) {
+            return null;
+        }
+
+        ChessPiece.PieceType pieceType = piece.getPieceType();
+
+        if (pieceType == PieceType.ROOK) {
+            ChessMoveFinder moveFinder = new RookMoveFinder();
+            return moveFinder.pieceMoves(board, myPosition);
+        }
+
+        return java.util.List.of();
     }
 
     @Override
