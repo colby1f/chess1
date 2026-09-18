6ab949337fb617c2c86631b666f43c1a6fe74198
Colby Fox <colbyfox@byu.edu>
2026-09-18 12:45:15 -0600

    implemented all the new movement spots and implemented move forward one

--- shared/src/main/java/chess/PawnMoveFinder.java
+++ shared/src/main/java/chess/PawnMoveFinder.java
@@ -1,4 +1,39 @@
 package chess;
 
-public class PawnMoveFinder {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PawnMoveFinder implements ChessMoveFinder {
+    public List<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        ChessGame.TeamColor pieceColor = board.getPiece(position).getTeamColor();
+
+        List<ChessMove> availableMoves = new ArrayList<>();
+
+        int promotion = 8;
+        int movement = 1;
+        if (pieceColor == ChessGame.TeamColor.BLACK){
+            promotion = 1;
+            movement = -1;
+        }
+
+        ChessPosition oneForward = new ChessPosition(position.getRow() + movement, position.getColumn());
+        ChessPosition twoForward = new ChessPosition(position.getRow() + movement * 2, position.getColumn());
+        ChessPosition leftDiagonal = new ChessPosition(position.getRow() + movement, position.getColumn() - 1);
+        ChessPosition rightDiagonal = new ChessPosition(position.getRow() + movement, position.getColumn() + 1);
+
+        if (board.getPiece(oneForward) == null){
+            if(oneForward.getRow() == 8){
+                availableMoves.add(new ChessMove(position, oneForward, ChessPiece.PieceType.QUEEN));
+                availableMoves.add(new ChessMove(position, oneForward, ChessPiece.PieceType.ROOK));
+                availableMoves.add(new ChessMove(position, oneForward, ChessPiece.PieceType.BISHOP));
+                availableMoves.add(new ChessMove(position, oneForward, ChessPiece.PieceType.KNIGHT));
+            } else{
+                availableMoves.add(new ChessMove(position, oneForward, null));
+            }
+
+        }
+
+
+        return availableMoves;
+    }
 }
