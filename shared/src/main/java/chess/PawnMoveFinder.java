49ad70b57f19cd324de344ab3cd7523d3df93f6e
Colby Fox <colbyfox@byu.edu>
2026-09-18 12:51:22 -0600

    implemented all the movements for pawn

--- shared/src/main/java/chess/PawnMoveFinder.java
+++ shared/src/main/java/chess/PawnMoveFinder.java
@@ -31,8 +31,36 @@ public class PawnMoveFinder implements ChessMoveFinder {
                 availableMoves.add(new ChessMove(position, oneForward, null));
             }
 
+            if(board.getPiece(twoForward) == null){
+                availableMoves.add(new ChessMove(position, twoForward, null));
+            }
         }
 
+        if (leftDiagonal.getColumn() >= 1) {
+            if (board.getPiece(leftDiagonal) == null) {
+                if (leftDiagonal.getRow() == 8) {
+                    availableMoves.add(new ChessMove(position, leftDiagonal, ChessPiece.PieceType.QUEEN));
+                    availableMoves.add(new ChessMove(position, leftDiagonal, ChessPiece.PieceType.ROOK));
+                    availableMoves.add(new ChessMove(position, leftDiagonal, ChessPiece.PieceType.BISHOP));
+                    availableMoves.add(new ChessMove(position, leftDiagonal, ChessPiece.PieceType.KNIGHT));
+                } else {
+                    availableMoves.add(new ChessMove(position, leftDiagonal, null));
+                }
+            }
+        }
+
+        if (rightDiagonal.getColumn() >= 1) {
+            if (board.getPiece(rightDiagonal) == null) {
+                if (rightDiagonal.getRow() == 8) {
+                    availableMoves.add(new ChessMove(position, rightDiagonal, ChessPiece.PieceType.QUEEN));
+                    availableMoves.add(new ChessMove(position, rightDiagonal, ChessPiece.PieceType.ROOK));
+                    availableMoves.add(new ChessMove(position, rightDiagonal, ChessPiece.PieceType.BISHOP));
+                    availableMoves.add(new ChessMove(position, rightDiagonal, ChessPiece.PieceType.KNIGHT));
+                } else {
+                    availableMoves.add(new ChessMove(position, rightDiagonal, null));
+                }
+            }
+        }
 
         return availableMoves;
     }
