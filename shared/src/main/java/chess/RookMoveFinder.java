e90c8ce6499159b59017f2d2ffad7c638b733544
Colby Fox <colbyfox@byu.edu>
2026-09-17 21:07:08 -0600

    fixed a couple errors in rook move finder

--- shared/src/main/java/chess/RookMoveFinder.java
+++ shared/src/main/java/chess/RookMoveFinder.java
@@ -13,15 +13,17 @@ public class RookMoveFinder implements ChessMoveFinder {
 
         for (int[] dir : directions) {
             for (int distance = 1; distance < 8; distance++) {
-                ChessPosition newPosition = new ChessPosition(position.getRow() + (dir[0] * distance), position.getColumn() + dir[0] * distance);
+                ChessPosition newPosition = new ChessPosition(position.getRow() + (dir[0] * distance), position.getColumn() + dir[1] * distance);
                 if (newPosition.getRow() > 8 || newPosition.getRow() < 1 || newPosition.getColumn() > 8 || newPosition.getColumn() < 1) {
                     break;
                 }
                 if (board.getPiece(newPosition) == null) {
                     availableMoves.add(new ChessMove(position, newPosition, null));
-                }
-                if (board.getPiece(newPosition).getTeamColor() != pieceColor) {
-                    availableMoves.add(new ChessMove(position, newPosition, null));
+                } else {
+                    if (board.getPiece(newPosition).getTeamColor() != pieceColor) {
+                        availableMoves.add(new ChessMove(position, newPosition, null));
+                        break;
+                    }
                     break;
                 }
             }
